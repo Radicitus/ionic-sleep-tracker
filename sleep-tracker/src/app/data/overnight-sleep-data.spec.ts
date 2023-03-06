92cbdf4add8bfafc5e318a3b844fb26d13cf8e75
Radicitus <novivolatus@gmail.com>
2023-03-06 00:26:20 -0800

    Add data and services from starter files

---
+++ sleep-tracker/src/app/data/overnight-sleep-data.spec.ts
@@ -0,0 +1,8 @@
+import { OvernightSleepData } from './overnight-sleep-data';
+
+describe('OvernightSleepData', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    expect(new OvernightSleepData()).toBeTruthy();
+  });
+});
---
+++ sleep-tracker/src/app/data/stanford-sleepiness-data.spec.ts
@@ -0,0 +1,8 @@
+import { StanfordSleepinessData } from './stanford-sleepiness-data';
+
+describe('StanfordSleepinessData', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    expect(new StanfordSleepinessData()).toBeTruthy();
+  });
+});
